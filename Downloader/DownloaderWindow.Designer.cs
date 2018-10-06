370c186b20cb0752b6b49bf42143aa03b225dc5c
Emmanuel Pericon <brianpericon.e92@gmail.com>
2018-10-06 17:01:49 -0300

    Add project files.

---
+++ Downloader/DownloaderWindow.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Downloader
+{
+    partial class DownloaderWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtProgress = new System.Windows.Forms.TextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtNombreArchivo = new System.Windows.Forms.TextBox();
+            this.btnDescargar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "URL a Descargar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Directorio/Nombre del archivo:";
+            // 
+            // txtProgress
+            // 
+            this.txtProgress.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProgress.Location = new System.Drawing.Point(16, 93);
+            this.txtProgress.Name = "txtProgress";
+            this.txtProgress.ReadOnly = true;
+            this.txtProgress.Size = new System.Drawing.Size(683, 19);
+            this.txtProgress.TabIndex = 2;
+            this.txtProgress.Text = "Esperando Usuario";
+            this.txtProgress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.Location = new System.Drawing.Point(238, 22);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(461, 26);
+            this.txtURL.TabIndex = 3;
+            // 
+            // txtNombreArchivo
+            // 
+            this.txtNombreArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreArchivo.Location = new System.Drawing.Point(238, 56);
+            this.txtNombreArchivo.Name = "txtNombreArchivo";
+            this.txtNombreArchivo.Size = new System.Drawing.Size(461, 26);
+            this.txtNombreArchivo.TabIndex = 4;
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescargar.Location = new System.Drawing.Point(502, 166);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(91, 36);
+            this.btnDescargar.TabIndex = 5;
+            this.btnDescargar.Text = "Descargar";
+            this.btnDescargar.UseVisualStyleBackColor = true;
+            this.btnDescargar.Click += new System.EventHandler(this.btnDescargar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(608, 166);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(91, 36);
+            this.btnDetener.TabIndex = 6;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 125);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(683, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // txtError
+            // 
+            this.txtError.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtError.ForeColor = System.Drawing.Color.Red;
+            this.txtError.Location = new System.Drawing.Point(12, 166);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.ReadOnly = true;
+            this.txtError.Size = new System.Drawing.Size(478, 36);
+            this.txtError.TabIndex = 9;
+            this.txtError.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DownloaderWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(711, 214);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnDescargar);
+            this.Controls.Add(this.txtNombreArchivo);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.txtProgress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "DownloaderWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtProgress;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtNombreArchivo;
+        private System.Windows.Forms.Button btnDescargar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtError;
+    }
+}
+
---
+++ Downloader/DownloaderWindow.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+namespace Downloader
+{
+    public partial class DownloaderWindow : Form
+    {
+        private Thread thrDownload;
+        private Stream strResponse;
+        private Stream strLocal;
+        private HttpWebRequest webRequest;
+        private HttpWebResponse webResponse;
+        private delegate void UpdateProgressCallback(Int64 bytesRead, Int64 totalBytes);
+        private static int PercentProgress;
+
+        public DownloaderWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void DownloaderWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateProgress(Int64 bytesRead, Int64 totalBytes)
+        {
+            PercentProgress = Convert.ToInt32((bytesRead * 100) / totalBytes);
+            this.progressBar.Value = PercentProgress;
+            txtProgress.Text = "Descargando " + bytesRead + " de " + totalBytes + " (" + PercentProgress + "%)";
+        }
+
+        private void Download()
+        {
+            using(WebClient wcDownload = new WebClient())
+            {
+                try
+                {
+                    webRequest = (HttpWebRequest)WebRequest.Create(this.txtURL.Text);
+                    webRequest.Credentials = CredentialCache.DefaultCredentials;
+                    webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    Int64 fileSize = webResponse.ContentLength;
+                    strResponse = wcDownload.OpenRead(this.txtURL.Text);
+                    strLocal = new FileStream(this.txtNombreArchivo.Text, FileMode.Create, FileAccess.Write, FileShare.None);
+                    int bytesSize = 0;
+                    byte[] downBuffer = new byte[2048];
+
+                    while((bytesSize = strResponse.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                    {
+                        strLocal.Write(downBuffer, 0, bytesSize);
+                        this.Invoke(new UpdateProgressCallback(this.UpdateProgress), new object[] { strLocal.Length, fileSize });
+
+                    }
+                }
+                finally
+                {
+                    strResponse.Close();
+                    strLocal.Close();
+                }
+            }
+        }
+
+        private void btnDescargar_Click(object sender, EventArgs e)
+        {
+            txtProgress.Text = "Descarga iniciada...";
+            thrDownload = new Thread(Download);
+            thrDownload.Start();
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            webResponse.Close();
+            strResponse.Close();
+            strLocal.Close();
+            thrDownload.Abort();
+            progressBar.Value = 0;
+            txtProgress.Text = "Descarga detenida...";
+        }
+        
+    }
+}
